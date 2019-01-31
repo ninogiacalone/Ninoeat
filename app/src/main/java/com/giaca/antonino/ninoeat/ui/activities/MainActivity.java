41ec86aeffff159429522fbb1b3ac81ce839d95f
ninogiacalone <antonino.giacalone9898@gmail.com>
2019-01-31 17:14:57 +0100

    3 commit

--- app/src/main/java/com/giaca/antonino/ninoeat/ui/activities/MainActivity.java
+++ app/src/main/java/com/giaca/antonino/ninoeat/ui/activities/MainActivity.java
@@ -1,8 +1,48 @@
 package com.giaca.antonino.ninoeat.ui.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.giaca.antonino.ninoeat.R;
+
 /**
  * Created by anton on 31/01/2019.
  */
 
-public class MainActivity {
-}
+public class MainActivity  extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.login_menu) {
+
+            startActivity(new Intent(this, LoginActivity.class));
+            return true;
+        } else if (item.getItemId() == R.id.checkout_menu) {
+
+            startActivity(new Intent(this, CheckoutActivity.class));
+        }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
