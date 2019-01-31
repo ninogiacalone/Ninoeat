51b262b9db674d861d085ee68b4779918b64609b
ninogiacalone <antonino.giacalone9898@gmail.com>
2019-01-31 16:27:16 +0100

    3 commit

--- app/src/main/java/com/giaca/antonino/ninoeat/ShopActivity.java
+++ app/src/main/java/com/giaca/antonino/ninoeat/ShopActivity.java
@@ -1,8 +1,19 @@
 package com.giaca.antonino.ninoeat;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by anton on 31/01/2019.
  */
 
-public class ShopActivity {
+public class ShopActivity  extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
 }
