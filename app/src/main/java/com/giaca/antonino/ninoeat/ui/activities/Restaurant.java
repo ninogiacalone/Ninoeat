5ba4c3990a0e874a7ec82fdfeb3b192d2a0e5f65
ninogiacalone <antonino.giacalone9898@gmail.com>
2019-02-05 16:12:03 +0100

    Commit of 05/02

--- app/src/main/java/com/giaca/antonino/ninoeat/ui/activities/Restaurant.java
+++ app/src/main/java/com/giaca/antonino/ninoeat/ui/activities/Restaurant.java
@@ -1,5 +1,7 @@
 package com.giaca.antonino.ninoeat.ui.activities;
 
+import android.media.Image;
+
 /**
  * Created by anton on 04/02/2019.
  */
@@ -8,12 +10,26 @@ public class Restaurant {
     String nome;
     String indirizzo;
     float prezzo;
+    String urlimm;
 
-    public Restaurant(String nome, String indirizzo, float prezzo) {
+
+    public Restaurant(String nome, String indirizzo, float prezzo,String urlimm) {
         this.nome = nome;
         this.indirizzo = indirizzo;
         this.prezzo = prezzo;
+        this.urlimm=urlimm;
     }
+
+    public String getUrlimm() {
+        return urlimm;
+    }
+
+    public void setUrlimm(String urlimm) {
+        this.urlimm = urlimm;
+    }
+
+
+
 
     public void setNome(String nome) {
         this.nome = nome;
--- app/src/main/java/com/giaca/antonino/ninoeat/ui/activities/adapters/Restaurant_adapters.java
+++ app/src/main/java/com/giaca/antonino/ninoeat/ui/activities/adapters/Restaurant_adapters.java
@@ -5,31 +5,44 @@ package com.giaca.antonino.ninoeat.ui.activities.adapters;
  */
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.giaca.antonino.ninoeat.R;
 import com.giaca.antonino.ninoeat.ui.activities.Restaurant;
+import com.giaca.antonino.ninoeat.ui.activities.ShopActivity;
 
 import java.util.ArrayList;
 
 public class Restaurant_adapters extends RecyclerView.Adapter {
 
+    private final Context context;
     private LayoutInflater inflater;
     private ArrayList<Restaurant> data;
+
 
 
 
     public Restaurant_adapters(Context context,ArrayList<Restaurant> data){
         inflater = LayoutInflater.from(context);
         this.data = data;
+        this.context=context;
+
 
 
     }
+
+
+
 
     @NonNull
     @Override
@@ -45,6 +58,9 @@ public class Restaurant_adapters extends RecyclerView.Adapter {
         vh.indirizzoName.setText(data.get(i).getIndirizzo());
         vh.prezzoMin.setText( "Prezzo minino "+data.get(i).getPrezzo());
 
+        Glide.with(context).load(data.get(i).getUrlimm()).into(vh.imm);
+
+
 
 
     }
@@ -56,24 +72,33 @@ public class Restaurant_adapters extends RecyclerView.Adapter {
 
 
 
-    public class RestaurantViewHolder extends RecyclerView.ViewHolder{
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public TextView restaurantName;
         public TextView indirizzoName;
         public TextView prezzoMin;
+        public Button menubtn;
+        public ImageView imm;
 
         public RestaurantViewHolder(@NonNull View itemView) {
             super(itemView);
             restaurantName = itemView.findViewById(R.id.nome);
             indirizzoName=itemView.findViewById(R.id.indirizzo);
             prezzoMin=itemView.findViewById(R.id.prezzomin);
+            menubtn=itemView.findViewById(R.id.btnmenu);
+            imm=itemView.findViewById(R.id.im);
+            menubtn.setOnClickListener(this);
 
 
 
         }
 
 
+        @Override
+        public void onClick(View view) {
+
+            if(view.getId()==R.id.btnmenu)
+                context.startActivity(new Intent(context,ShopActivity.class));
+        }
     }
-
-
-}
+  }
